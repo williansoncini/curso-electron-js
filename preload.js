632813e94b1ca5df0304242618bbe924e7f4e829
Willian <willian_soncini@hotmail.com>
2022-01-18 16:51:45 -0300

    fix: Solucionando problema de não aparecer versões

--- preload.js
+++ preload.js
@@ -1,11 +1,11 @@
-window.addEventListener('DOMContendLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element)
-            element.innerText = text
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
     }
-
-    for (const dependency of ['chrome', 'node', 'electron']){
-        replaceText(`${dependency}-version`, process.versions[dependency])
+  
+    for (const type of ['chrome', 'node', 'electron']) {
+      replaceText(`${type}-version`, process.versions[type])
     }
-})
+  })
+  
